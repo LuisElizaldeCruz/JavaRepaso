dbcadb1612eeb5010fdd0c117226e7863f7eb40c
luis elizalde <luis.elizalde.dev@gmail.com>
2023-11-19 18:42:12 -0600

    se creo el patro singleton

---
+++ src/patrones_diseno/ConexionBDSingleton.java
@@ -0,0 +1,15 @@
+package patrones_diseno;
+
+public class ConexionBDSingleton {
+    private static ConexionBDSingleton instancia;
+    private ConexionBDSingleton() {//declarando el constructor como privado se evita llamar multiples veces
+        System.out.println("Conectandose a un motor de base de datos");
+    }
+
+    public static ConexionBDSingleton  getInstancia() {
+        if (instancia == null){
+            instancia = new ConexionBDSingleton();
+        }
+        return instancia;
+    }
+}
